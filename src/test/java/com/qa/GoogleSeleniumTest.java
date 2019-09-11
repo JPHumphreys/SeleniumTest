795b9e7453ace26ba6805e584f244d36aee972a5
Jonathan Humphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-11 10:04:43 +0000

    created a search test to find cat pictures

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -2,7 +2,12 @@ package com.qa;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSeleniumTest {
 
@@ -10,7 +15,7 @@ public class GoogleSeleniumTest {
 
     @Before
     public void setUp(){
-        System.setProperty("web.chrome.driver","resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\IdeaProjects\\SeleniumTesting\\src\\test\\java\\resources\\chromedriver.exe");
 
         driver = new ChromeDriver();
 
@@ -22,6 +27,23 @@ public class GoogleSeleniumTest {
     @After
     public void tearDown(){
         driver.close();
+    }
+
+
+    @Test
+    public void searchTest() throws InterruptedException {
+        driver.manage().window().maximize();//max screen
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        WebElement searchField = driver.findElement(By.name("q"));
+        assertTrue(searchField.isDisplayed());
+        searchField.sendKeys("funny cat pictures");
+        Thread.sleep(5000);
+        WebElement submitButton = driver.findElement(By.name("btnK"));
+        submitButton.click();
+        Thread.sleep(5000);
+
+
     }
 
 }
