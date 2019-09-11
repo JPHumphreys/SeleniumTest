ccf12dde151c9f7b8f369d3dee73565b2fae2fd4
Jonathan Humphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-11 09:48:56 +0000

    added chrome driver in before and removed in after class

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -1,8 +1,29 @@
 package com.qa;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleSeleniumTest {
 
     private ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("web.chrome.driver","resources/chromedriver.exe");
+
+        driver = new ChromeDriver();
+
+
+    }
+
+
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
 }
+
+
