74c2e9a8513324543a367cf16c8671d944108a33
Jonathan Humphreys <Jonathan.Humphreys@academytrainee.com>
2019-09-11 10:32:04 +0000

    created a scroller for a page

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,12 +13,15 @@ import static org.junit.Assert.assertTrue;
 public class GoogleSeleniumTest {
 
     private ChromeDriver driver;
+    private JavascriptExecutor js;
 
     @Before
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\IdeaProjects\\SeleniumTesting\\src\\test\\java\\resources\\chromedriver.exe");
 
         driver = new ChromeDriver();
+
+        js = (JavascriptExecutor) driver;
 
 
     }
@@ -49,6 +53,14 @@ public class GoogleSeleniumTest {
         linkToBiggerPictures.click();
         Thread.sleep(5000);
 
+        int counter = 1;
+        for(int i = 0; i < 100; i++)
+        {
+            js.executeScript("window.scrollBy(" + i + "," + (counter++) +")"); //Scroll vertically down by 1000 pixels
+            Thread.sleep(50);
+        }
+
+        Thread.sleep(1000);
 
 
 
